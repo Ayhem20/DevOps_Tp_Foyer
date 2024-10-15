766b53b1c77ca43e780b76b3a0391348bbe59128
ayhem <ayhem.boughdiri@esprit.tn>
2024-10-15 12:47:06 +0100

    projet foyer sonarqube phase

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,45 @@
 pipeline {
     agent any
+
     stages {
-        stage('Checkout GIT') {
+        stage('Checkout Code') {
             steps {
-                echo 'Pulling...'
-                    git branch: 'ayhem', 
-                    url: 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git',
-                    credentialsId: '8f9b2f59-5031-4710-ba76-f57fadc1a5de'
+                script {
+                    def repoUrl = 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git'
+                    def branch = 'ayhem' // Your branch name
+                    checkout([$class: 'GitSCM', 
+                        branches: [[name: "*/${branch}"]], 
+                        userRemoteConfigs: [[url: repoUrl]]
+                    ])
+                }
             }
         }
-        stage('Maven Build') {
+
+        stage('Maven Clean and Compile') {
             steps {
-                sh 'mvn clean compile'
+                script {
+                    sh 'mvn clean compile'
+                }
             }
         }
+
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    // Assurez-vous que les variables d'environnement nécessaires sont définies
+                    withCredentials([usernamePassword(credentialsId: '71c996e3-a2eb-47ae-b081-f6d349669f6d', usernameVariable: 'admin', passwordVariable: 'Magrouch123!')]) {
+                        sh '''
+                        mvn sonar:sonar \
+                          -Dsonar.projectKey=DevOps_Tp_Foyer \
+                          -Dsonar.host.url=http://192.168.33.10:9000 \
+                          -Dsonar.login=$SONAR_USER \
+                          -Dsonar.password=$SONAR_PASSWORD
+                        '''
+                    }
+                }
+            }
+        }
+
+        // Ajoutez d'autres stages ici si nécessaire
     }
 }
