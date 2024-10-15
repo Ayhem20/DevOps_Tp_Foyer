40e5eb36c7ea8f0ac0fa9c3d0942bce11f97126e
ayhem <ayhem.boughdiri@esprit.tn>
2024-10-15 13:10:45 +0100

    projet foyer sonarqube phase

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,17 @@
 pipeline {
     agent any
 
+    stages {
         stage('Checkout Code') {
             steps {
                 script {
                     def repoUrl = 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git'
                     def branch = 'ayhem'
-                withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
-                    checkout([$class: 'GitSCM',
-                        branches: [[name: "*/${branch}"]],
-                        userRemoteConfigs: [[url: "${repoUrl}", credentialsId: '8f9b2f59-5031-4710-ba76-f57fadc1a5de']]
+                    def github-token = '8f9b2f59-5031-4710-ba76-f57fadc1a5de'
+                    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+                        checkout([$class: 'GitSCM',
+                            branches: [[name: "*/${branch}"]],
+                            userRemoteConfigs: [[url: "${repoUrl}", credentialsId: 'github-token']]
                         ])
                     }
                 }
@@ -27,19 +29,19 @@ pipeline {
         stage('SonarQube Analysis') {
             steps {
                 script {
-                    // Assurez-vous que les variables d'environnement nécessaires sont définies
-                    withCredentials([usernamePassword(credentialsId: '71c996e3-a2eb-47ae-b081-f6d349669f6d', usernameVariable: 'admin', passwordVariable: 'Magrouch123!')]) {
-                        sh '''
-                        mvn sonar:sonar \
-                          -Dsonar.projectKey=DevOps_Tp_Foyer \
-                          -Dsonar.host.url=http://192.168.33.10:9000 \
-                          -Dsonar.login=$SONAR_USER \
-                          -Dsonar.password=$SONAR_PASSWORD
-                        '''
-                    }
+                    // SonarQube credentials
+                    def sonarUser = 'admin'
+                    def sonarPassword = 'Magrouch123!'
+                    
+                    sh '''
+                    mvn sonar:sonar \
+                      -Dsonar.projectKey=DevOps_Tp_Foyer \
+                      -Dsonar.host.url=http://192.168.33.10:9000 \
+                      -Dsonar.login=${sonarUser} \
+                      -Dsonar.password=${sonarPassword}
+                    '''
                 }
             }
         }
-
-        // Ajoutez d'autres stages ici si nécessaire
+    }
 }
