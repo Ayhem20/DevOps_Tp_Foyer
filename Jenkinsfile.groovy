3e642ace19ad0611ebcd8f18a31b15a190702c0f
chaima sassi <sassi.chaima@esprit.tn>
2024-10-30 16:59:43 +0100

    deploy nexus3

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,35 +9,37 @@ pipeline {
                     def branch = 'chaima-sassi'
                     withCredentials([usernamePassword(credentialsId: 'c38cdad7-ffdc-4cf7-a254-95950ce5c532', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
                         checkout([$class: 'GitSCM',
-                            branches: [[name: "${branch}"]], // Changed 'branch' to 'name'
+                            branches: [[name: "${branch}"]],
                             userRemoteConfigs: [[url: "${repoUrl}", credentialsId: 'c38cdad7-ffdc-4cf7-a254-95950ce5c532']]
                         ])
                     }
                 }
             }
         }
-      stage('Maven Clean and Compile') {
+        
+        stage('Maven Clean and Compile') {
             steps {
                 script {
                     sh 'mvn clean compile'
                 }
             }
         }
-   stage('SonarQube Analysis') {
-    steps {
-        script {
-            withCredentials([string(credentialsId: '0b0a7f3b-d5e8-40bc-8fd9-3a469de95804', variable: 'SONAR_TOKEN')]) {
-                sh '''
-                mvn sonar:sonar \
-                  -Dsonar.projectKey=DevOps_Tp_Foyer \
-                  -Dsonar.host.url=http://192.168.56.4:9000 \
-                  -Dsonar.login=$SONAR_TOKEN
-                '''
+
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    withCredentials([string(credentialsId: '0b0a7f3b-d5e8-40bc-8fd9-3a469de95804', variable: 'SONAR_TOKEN')]) {
+                        sh '''
+                        mvn sonar:sonar \
+                          -Dsonar.projectKey=DevOps_Tp_Foyer \
+                          -Dsonar.host.url=http://192.168.56.4:9000 \
+                          -Dsonar.login=$SONAR_TOKEN
+                        '''
+                    }
+                }
             }
         }
-    }
-}
-stages {
+        
         stage('Deploy to Nexus') {
             steps {
                 script {
@@ -60,8 +62,13 @@ stages {
             }
         }
     }
-}
- 
 
-    
+    post {
+        always {
+            echo 'Pipeline completed.'
+        }
+        failure {
+            echo 'Pipeline failed!'
+        }
+    }
 }
