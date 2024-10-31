bc87f2e054bb11ebbcae879ca6faf5d9a7e0e2ce
chaima sassi <sassi.chaima@esprit.tn>
2024-10-31 10:43:17 +0100

    second day nexuss

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -54,7 +54,7 @@ pipeline {
             def jarFile = 'target/tp-foyer-5.0.0.jar'
             
             if (fileExists(jarFile)) {
-                withCredentials([usernamePassword(credentialsId: 'f9ee6567-4d1b-4502-9a4c-a3a0c19dbd0e', usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
+                withCredentials([usernamePassword(credentialsId: '6caa081d-c871-4a56-9fe4-a5b70bafaa0b', usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
                     sh """
                         mvn deploy:deploy-file \
                           -Durl=http://192.168.56.4:8081/repository/maven-releases/ \
