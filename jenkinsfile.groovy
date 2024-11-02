5ef4df934cbfff0731bf1484bfa37e9f2e1faa08
ibtihel garreb <garreb.ibtihel@esprit.tn>
2024-11-02 18:10:10 +0100

    Mise à jour de jenkinsfile.groovy avec l'intégration SonarQube

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -47,12 +47,12 @@ pipeline {
         stage('SonarQube Analysis') {
             steps {
                 script {
-                    withCredentials([string(credentialsId: 'SONAR_TOKEN1', variable: 'SONAR_TOKEN1')]) {
+                    withCredentials([string(credentialsId: 'SONAR_TOKEN1', variable: 'SONAR_TOKEN')]) {
                         sh '''
                         mvn sonar:sonar \
                           -Dsonar.projectKey=DevOps_Tp_Foyer \
                           -Dsonar.host.url=http://localhost:9001 \
-                          -Dsonar.login=$SONAR_TOKEN1
+                          -Dsonar.login=$SONAR_TOKEN
                         '''
                     }
                 }
