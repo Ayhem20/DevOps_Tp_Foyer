220733aba342dc4f48ab4c85aae06fc98b27f129
ibtihel garreb <garreb.ibtihel@esprit.tn>
2024-11-10 18:35:17 +0100

    add

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,68 +28,55 @@ pipeline {
         // Étape pour nettoyer et compiler avec Maven
         stage('Maven Clean and Compile') {
             steps {
-                script {
-                    sh 'mvn clean compile'
-                }
+                sh 'mvn clean compile'
             }
         }
 
         // Étape pour empaqueter l'application dans un JAR avec Maven
         stage('Maven Package') {
             steps {
-                script {
-                    sh 'mvn package'
-                }
+                sh 'mvn package'
             }
         }
 
         // Étape pour analyser le code avec SonarQube
-       
-         stage('MVN SONARQUBE') {
+        stage('MVN SONARQUBE') {
             steps {
                 withCredentials([string(credentialsId: 'c27ceaee-5193-4a7c-988b-22150e0c3e9d', variable: 'SONAR_TOKEN')]) {
                     sh 'mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN'
                 }
             }
         }
-        
-          // Stage to deploy to Nexus
+
+        // Étape pour déployer sur Nexus
         stage('Deploy to Nexus') {
             steps {
-                script {
-                    withCredentials([usernamePassword(credentialsId: 'a278b6b7-0941-4825-a111-0459663e9bc5', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASSWORD')]) {
-                        sh '''
-                          mvn clean deploy -DskipTests
-                        '''
-                    }
+                withCredentials([usernamePassword(credentialsId: 'a278b6b7-0941-4825-a111-0459663e9bc5', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASSWORD')]) {
+                    sh 'mvn clean deploy -DskipTests'
                 }
             }
-         // Stage to build the Docker image
+        }
+
+        // Étape pour construire l'image Docker
         stage('Building Image') {
             steps {
                 script {
-                    // Build the Docker image
+                    // Construire l'image Docker
                     sh 'docker build -t ibtihelgr/alpine:latest .'
                 }
             }
         }
 
-        // Stage to deploy the Docker image to DockerHub
+        // Étape pour déployer l'image Docker sur DockerHub
         stage('Deploy Image') {
             steps {
-                script {
-                    withCredentials([usernamePassword(credentialsId: '57d779b6-7114-4142-977c-9b93fff27676', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                        sh '''
-                            docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-                            docker push ibtihelgr/alpine:latest
-                        '''
-                    }
+                withCredentials([usernamePassword(credentialsId: '57d779b6-7114-4142-977c-9b93fff27676', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                    sh '''
+                        docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+                        docker push ibtihelgr/alpine:latest
+                    '''
                 }
             }
         }
-        
-
     }
-    }
-    }
-
+}
