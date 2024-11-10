41a6193ce75222c2dbbfddeb97b391192d17bf22
chamssdin <chamsdine.madouri@esprit.tn>
2024-11-11 00:45:57 +0100

    git

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+
+pipeline {
+    agent any
+
+
+    stages {
+        // Stage to check out the code from GitHub
+        stage('Checkout Code') {
+            steps {
+                script {
+                    def repoUrl = 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git'
+                    def branch = 'chamsdine_madouri'
+                    withCredentials([usernamePassword(credentialsId: '87706fb2-7071-42b7-94df-fb3b77774c16', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
+                        checkout([$class: 'GitSCM',
+                            branches: [[name: "${branch}"]],
+                            userRemoteConfigs: [[url: "${repoUrl}", credentialsId: '87706fb2-7071-42b7-94df-fb3b77774c16']]
+                        ])
+                    }
+                }
+            }
+        }
+}
+}
