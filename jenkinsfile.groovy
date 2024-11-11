d30ae61816c71fd21f885edaa858e9829ffce294
ibtihel garreb <garreb.ibtihel@esprit.tn>
2024-11-11 16:08:50 +0100

    Ajout de jenkinsfile.groovy depuis la branche ibtihel vers ibtihelreservation

---
+++ jenkinsfile.groovy
@@ -0,0 +1,111 @@
+pipeline {
+    agent any
+
+    stages {
+        // Étape pour configurer Git
+        stage('Setup Git Config') {
+            steps {
+                sh 'git config --global http.postBuffer 524288000'
+            }
+        }
+
+        // Étape pour récupérer le code de GitHub
+        stage('Checkout Code') {
+            steps {
+                script {
+                    def repoUrl = 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git'
+                    def branch = 'ibtihel'
+                    withCredentials([usernamePassword(credentialsId: '608aa325-9043-49fb-afb2-6e953ec29495', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
+                        checkout([$class: 'GitSCM',
+                            branches: [[name: "${branch}"]],
+                            userRemoteConfigs: [[url: "${repoUrl}", credentialsId: '608aa325-9043-49fb-afb2-6e953ec29495']]
+                        ])
+                    }
+                }
+            }
+        }
+
+        // Étape pour nettoyer et compiler avec Maven
+        stage('Maven Clean and Compile') {
+            steps {
+                script {
+                    sh 'mvn clean compile'
+                }
+            }
+        }
+
+        // Étape pour empaqueter l'application dans un JAR avec Maven
+        stage('Maven Package') {
+            steps {
+                script {
+                    sh 'mvn package'
+                }
+            }
+        }
+
+        // Étape pour analyser le code avec SonarQube
+        stage('MVN SONARQUBE') {
+            steps {
+                withCredentials([string(credentialsId: 'c27ceaee-5193-4a7c-988b-22150e0c3e9d', variable: 'SONAR_TOKEN')]) {
+                    sh 'mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN'
+                }
+            }
+        }
+
+        // Étape pour déployer sur Nexus
+        stage('Deploy to Nexus') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'a278b6b7-0941-4825-a111-0459663e9bc5', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASSWORD')]) {
+                        sh 'mvn clean deploy -DskipTests'
+                    }
+                }
+            }
+        }
+
+        // Étape pour construire l'image Docker
+        stage('Building Image') {
+            steps {
+                script {
+                    sh 'docker build -t ibtihelgr/alpine:latest .'
+                }
+            }
+        }
+
+        // Étape pour pré-télécharger les images nécessaires (gestion du timeout)
+        stage('Pre-Pull Docker Images') {
+            steps {
+                script {
+                    // Remplacez "myapp_image:latest" par le nom de votre image si elle est manquante
+                    sh 'docker pull ibtihelgr/alpine:latest || true'
+                    // Vous pouvez ajouter ici d'autres images à pré-télécharger si besoin
+                }
+            }
+        }
+
+        // Étape pour déployer l'image Docker sur DockerHub
+        stage('Deploy Image') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: '57d779b6-7114-4142-977c-9b93fff27676', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh '''
+                            docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+                            docker push ibtihelgr/alpine:latest
+                        '''
+                    }
+                }
+            }
+        }
+
+        // Étape Docker Compose
+        stage('Docker Compose') {
+            steps {
+                script {
+                    echo "Running Docker Compose"
+                    // Lancer les conteneurs en arrière-plan
+                    sh 'docker compose up -d'
+                }
+            }
+        }
+    }
+}
