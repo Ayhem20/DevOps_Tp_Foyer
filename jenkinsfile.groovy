3914248e9ee9363e817e4686e6f6e10a61301faa
ibtihelgarreb <146930569+ibtihelgarreb@users.noreply.github.com>
2024-11-13 09:08:15 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -15,10 +15,10 @@ pipeline {
                 script {
                     def repoUrl = 'https://github.com/Ayhem20/DevOps_Tp_Foyer.git'
                     def branch = 'ibtihel'
-                    withCredentials([usernamePassword(credentialsId: '608aa325-9043-49fb-afb2-6e953ec29495', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
+                    withCredentials([usernamePassword(credentialsId: '', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
                         checkout([$class: 'GitSCM',
                             branches: [[name: "${branch}"]],
-                            userRemoteConfigs: [[url: "${repoUrl}", credentialsId: '608aa325-9043-49fb-afb2-6e953ec29495']]
+                            userRemoteConfigs: [[url: "${repoUrl}", credentialsId: '']]
                         ])
                     }
                 }
